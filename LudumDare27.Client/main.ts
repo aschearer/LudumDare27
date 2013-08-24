d2c388d6fba673f842b93c77de662e706170ba2d
Alex Schearer <aschearer@gmail.com>
2013-08-23 19:15:13 -0700

    Adding skeleton project

---
+++ LudumDare27.Client/main.ts
@@ -0,0 +1,16 @@
+/// <reference path="libs/typings/requirejs/require.d.ts"/>
+
+require.config({
+    paths: {
+        'jquery': 'libs/jquery.min',
+        'easel': 'libs/createjs-min'
+    },
+    shim: {
+        jquery: {
+            exports: '$'
+        },
+        easel: {
+            exports: 'createjs' 
+        }
+    }
+});
