f6ecf01445bc9971a80eddcf047e2c71bd5241d8
Justin <justin@lordpi.com>
2013-08-26 05:17:37 -0700

    Early help text idea

--- LudumDare27.Client/views/states/help.js
+++ LudumDare27.Client/views/states/help.js
@@ -6,17 +6,41 @@ var views;
         var Help = (function () {
             function Help(datacontext) {
                 this.id = "views.states.Help";
+                this.currentInstruction = -1;
                 this.datacontext = datacontext;
                 this.layer = document.getElementById('help-layer');
+                this.instructions = this.layer.getElementsByClassName('instructions')[0];
             }
             Help.prototype.enter = function (previousState) {
-                //this.backButton.onclick = (event) => {
-                //    this.datacontext.goBack();
-                //};
+                this.currentInstruction = 0;
+                this.activateInstruction(this.currentInstruction);
             };
 
             Help.prototype.exit = function (nextState) {
-                //this.backButton.onclick = null;
+                if (this.activeButton) {
+                    this.activeButton.onclick = null;
+                }
+            };
+
+            Help.prototype.activateInstruction = function (activeInstruction) {
+                var _this = this;
+                if ((this.activeInstruction != null) && this.activeInstruction.classList.contains('active-instruction')) {
+                    this.activeInstruction.classList.remove('active-instruction');
+                }
+                if (this.activeButton) {
+                    this.activeButton.onclick = null;
+                }
+
+                if (activeInstruction >= this.instructions.children.length) {
+                    this.datacontext.goBack();
+                } else {
+                    this.activeInstruction = this.instructions.children[activeInstruction];
+                    this.activeInstruction.classList.add('active-instruction');
+                    this.activeButton = this.activeInstruction.getElementsByClassName('more-button')[0];
+                    this.activeButton.onclick = function () {
+                        _this.activateInstruction(activeInstruction + 1);
+                    };
+                }
             };
             return Help;
         })();
